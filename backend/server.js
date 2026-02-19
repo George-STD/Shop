cf1ec1dd2d398387227f37770e394e613e95ac99
George-STD <2023170173@cis.asu.edu.eg>
2026-02-19 20:31:37 +0200

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ["https://shop-ten-liart.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
